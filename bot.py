f566ba7279cf424a670c3e8291409707a0e0a40a
malikilamalik <malikilamalik@student.telkomuniversity.ac.id>
2020-09-22 16:25:02 +0700

    Basic commands

---
+++ bot.py
@@ -0,0 +1,93 @@
+import discord
+from discord.ext import commands
+
+client = commands.Bot(command_prefix = commands.when_mentioned_or('!'))
+client.remove_command('help')
+#Event
+#Displaying if bot ready
+@client.event
+async def on_ready():
+    print('Bot is ready.')
+
+@client.event
+async def on_command_error(ctx,error):
+    if isinstance(error,commands.CommandNotFound):
+        await ctx.send('Perintah tidak ada')
+#Commands
+#Bot latency
+@client.command()
+async def ping(ctx):
+    await ctx.send(f'Pong! {round(client.latency * 1000)} ms')
+
+#bot call
+@client.command(aliases=['vanderlinde'])
+async def Vanderlinde(ctx):
+    await ctx.send('Gunakan !help untuk melihat commands list')
+
+#Help list
+@client.command(aliases=['commands'],pass_context=True)
+async def help(ctx):
+    author = ctx.message.author
+
+    embed = discord.Embed(
+        colour = discord.Colour.red()
+    )
+
+    embed.set_author(name='Help')
+    embed.add_field(name='!help', value = 'Bot commands list', inline=False)
+    embed.add_field(name='!ping', value = 'Bot latency!', inline=False)
+    embed.add_field(name='!registration', value = 'Returns registration list', inline=False)
+    embed.add_field(name='!accepted', value = 'Returns accepted list', inline=False)
+    embed.add_field(name='!rejected', value = 'Returns rejected list', inline=False)
+    embed.add_field(name='!detail <id>', value = 'Registration detail!', inline=False)
+    embed.add_field(name='!accept <id>', value = 'Accepting registration', inline=False)
+    embed.add_field(name='!reject <id>', value = 'Rejecting registration', inline=False)
+
+    await author.send(embed=embed)
+    
+#Registration list
+@client.command(aliases=['registration'])
+async def registration_list(ctx):
+    await ctx.send('Vanderlinde')
+
+#Accepted list
+@client.command(aliases=['accepted'])
+async def accepted_list(ctx):
+    await ctx.send('Accepted')
+
+#Rejected list
+@client.command(aliases=['rejected'])
+async def rejected_list(ctx):
+    await ctx.send('Rejected')
+
+#Registration detail
+@client.command(aliases=['detail'])
+async def registration_detail(ctx, id = 0):
+    if(id == 0):
+        await ctx.send('mohon isi detail id')
+    else:
+        await ctx.send('yo wassap mafren')
+
+#Accepted
+@client.command(aliases=['accept'])
+async def accept_registration(ctx, id = 0):
+    if(id == 0):
+        await ctx.send('mohon isi detail id')
+    else:
+        await ctx.send('aye aye aye aye')
+
+#Rejected
+@client.command(aliases=['reject'])
+async def reject_registration(ctx, id = 0):
+    if(id == 0):
+        await ctx.send('mohon isi detail id')
+    else:
+        await ctx.send('muda muda muda muda')
+
+#Say
+@client.command(aliases=['say'])
+async def bot_say(ctx, *, word):
+    author = ctx.message.author
+    await ctx.send(f'{author.mention} says {word}')
+
+client.run('TOKEN')
